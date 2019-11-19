0e259c5702da289c80a4ed0626c9a3f8b54d91b2
MacsedProtoss <yu410420@gmail.com>
2019-11-19 20:56:36 +0800

    merge rabbit's commit

---
+++ src/group/Macsed/TankBattle/rank/RankList.java
@@ -0,0 +1,23 @@
+package group.Macsed.TankBattle.rank;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RankList implements Serializable {
+    private static final long serialVersionUID = 7247714666080613254L;
+
+    private List<RankItem> rankItemList;
+
+    public RankList() {
+        this.rankItemList = new ArrayList<>();
+    }
+
+    public List<RankItem> getRankList() {
+        return rankItemList;
+    }
+
+    public void setRankList(List<RankItem> rankItemList) {
+        this.rankItemList = rankItemList;
+    }
+}
---
+++ src/group/Macsed/TankBattle/rank/rank_example.java
@@ -0,0 +1,22 @@
+package group.Macsed.TankBattle;
+
+import group.Macsed.TankBattle.rank.RankItem;
+import group.Macsed.TankBattle.rank.RankUtils;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class rank_example {
+    public void example() throws IOException, ClassNotFoundException {
+        RankUtils rankUtils = new RankUtils();
+        rankUtils.readRankList();
+        RankItem rankItem = new RankItem();
+
+        rankItem.setDate(new Date());
+        rankItem.setUsername("hzytql");
+        rankItem.setScore(100);
+
+        rankUtils.getRankList().add(rankItem);
+        rankUtils.saveRankList();
+    }
+}
