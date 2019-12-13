6b77ae501db0931ff29e2391cfb4f8864ee747d3
ttzztztz <935319340@qq.com>
2019-12-13 18:42:49 +0800

    Add Credits System

--- src/group/Macsed/TankBattle/Model/Rank/RankUtils.java
+++ src/group/Macsed/TankBattle/Model/Rank/RankUtils.java
@@ -1,10 +1,12 @@
 package group.Macsed.TankBattle.Model.Rank;
 
 import java.io.*;
+import java.util.Date;
 import java.util.List;
 
 public class RankUtils {
     private RankList rankList;
+    private final Object rankListLock = new Object();
 
     public RankUtils() {
         this.rankList = new RankList();
@@ -18,12 +20,26 @@ public class RankUtils {
         this.rankList.setRankList(rankList);
     }
 
+    public void insertRankList(String username, Integer credits, Date date) throws ClassNotFoundException, IOException {
+        synchronized (rankListLock) {
+            this.readRankList();
+
+            RankItem rankItem = new RankItem();
+            rankItem.setUsername(username);
+            rankItem.setScore(credits);
+            rankItem.setDate(date);
+
+            this.getRankList().add(rankItem);
+            this.saveRankList();
+        }
+    }
+
     public void readRankList() throws ClassNotFoundException {
         try {
             InputStream inputStream = new FileInputStream("./rank_list.rabbit");
             ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
             this.rankList = (RankList) objectInputStream.readObject();
-        } catch (IOException exception ) {
+        } catch (IOException exception) {
             this.rankList = new RankList();
         }
     }
